fff9f3fdadda89b71c57f9795e4172b8db3bb216
Cristhian Palomino Vega <paloominounprg@gmail.com>
2019-01-16 11:23:01 -0500

    model migration controllers

---
+++ app/Http/Controllers/BloqueController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\model\bloque;
+use Illuminate\Http\Request;
+
+class BloqueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\model\bloque  $bloque
+     * @return \Illuminate\Http\Response
+     */
+    public function show(bloque $bloque)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\model\bloque  $bloque
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(bloque $bloque)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\model\bloque  $bloque
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, bloque $bloque)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\model\bloque  $bloque
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(bloque $bloque)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PrestamoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\model\prestamo;
+use Illuminate\Http\Request;
+
+class PrestamoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\model\prestamo  $prestamo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(prestamo $prestamo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\model\prestamo  $prestamo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(prestamo $prestamo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\model\prestamo  $prestamo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, prestamo $prestamo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\model\prestamo  $prestamo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(prestamo $prestamo)
+    {
+        //
+    }
+}
