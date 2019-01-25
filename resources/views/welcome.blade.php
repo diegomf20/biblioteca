2cf393a1e0d92a7e6dedb4c1ac190048f01e3138
Cristhian Palomino Vega <paloominounprg@gmail.com>
2019-01-25 14:41:18 -0500

    din 7

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,17 @@
     .home-flujo img{
         width: 101%;
     }
+    .home-flujo{
+        margin: 0;
+        background-image: url('/img/homee.png');
+        background-size: 100% 100%;
+        /* background-position: center; */
+        background-repeat: no-repeat;
+        height: calc(100vh - 94px);
+    }
+
 </style>
 <div class=home-flujo>
-    <img src="{{asset('img/homee.png')}}" class="imagen-presentacion">
+    {{-- <img src="{{asset('img/homee.png')}}" class="imagen-presentacion"> --}}
 </div>
 @endsection
